70e8f0c09c04c35cba287dbed2c5edd99bc2807a
Tuigun Bekishev <tbekishev@gmail.com>
2022-12-16 14:57:43 -0400

    add user login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create] 
   end
+  resources :users, only: [:new, :create]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
