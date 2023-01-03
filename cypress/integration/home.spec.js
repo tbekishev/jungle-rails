dd55df160a33dde0fef9959aa2ab394cf62a4f5a
Tuigun Bekishev <tbekishev@gmail.com>
2023-01-03 19:00:44 -0400

    add product display tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('Feature Spec:', () => {
+
+  it('opens the homepage', () => {
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
